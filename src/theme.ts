773fb7ebf3d359136eac575b1145b20201c688fb
berdal <leonidas131@hotmail.com>
2022-12-31 16:27:53 +0300

    first init

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+import { green } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#4B516F",
+    },
+    secondary: {
+      main: "#4B9EFE",
+    },
+  },
+});
+export default theme;
