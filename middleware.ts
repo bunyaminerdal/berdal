9239436fa1e727212ac3050e64bfa2ba5edfb35e
berdal <leonidas131@hotmail.com>
2023-03-26 23:06:00 +0300

    no middleware

--- middleware.ts
+++ middleware.ts
@@ -3,18 +3,18 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
   // Get the pathname of the request (e.g. /, /protected)
-  const path = req.nextUrl.pathname;
+  // const path = req.nextUrl.pathname;
 
-  const session = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-  //TODO: if we need to protect a page, we can check session
-  //   if (!session && path === "/protected") {
-  //     return NextResponse.redirect(new URL("/login", req.url));
-  //   } else
-  if (session && (path === "/login" || path === "/register")) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+  // const session = await getToken({
+  //   req,
+  //   secret: process.env.NEXTAUTH_SECRET,
+  // });
+  // //TODO: if we need to protect a page, we can check session
+  // //   if (!session && path === "/protected") {
+  // //     return NextResponse.redirect(new URL("/login", req.url));
+  // //   } else
+  // if (session && (path === "/login" || path === "/register")) {
+  //   return NextResponse.redirect(new URL("/", req.url));
+  // }
   return NextResponse.next();
 }
