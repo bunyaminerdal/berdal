4d48c627c9a3e080dff519f7dbb2b3149ae5f819
bunyaminerdal <bunyaminerdal86@gmail.com>
2025-05-05 20:39:39 +0300

    🐛 theme fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,14 @@
-import * as React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import createEmotionCache from "../src/createEmotionCache";
-import { useCustomTheme } from "../src/useTheme";
 
 export default class MyDocument extends Document {
-  theme = useCustomTheme("light");
+  // theme = useCustomTheme("light");
   render() {
     return (
       <Html lang="en">
         <Head>
-          <meta name="theme-color" content={this.theme.palette.primary.main} />
+          {/* <meta name="theme-color" content={this.theme.palette.primary.main} /> */}
           <link rel="shortcut icon" href="/berdal.ico" />
           <link
             rel="stylesheet"
